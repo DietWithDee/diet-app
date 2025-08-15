3186ff8b585c514725ee71d1c4e2b708f259c424
Prince Tetteh <princetetteh963@gmail.com>
2025-08-15 00:46:15 +0000

    route layout update

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 function Error() {
   return (
-    <div className='text-red-500'>
+    <div className='py-12 min-h-70vh bg-gradient-to-br from-green-50 via-white to-emerald-50 text-center items-center'>
+      <div className=' text-4xl lg:text-7xl font-black text-black leading-tight'>
         This Page Not Found
+        <br />
+        404.....
+      </div>
         </div>
   )
 }
